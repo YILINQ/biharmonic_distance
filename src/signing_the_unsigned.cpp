a53c3cbff3bba10d62daa56bd2141c33ae28f827
Yilin <yilin@Yilins-MacBook-Pro.local>
2020-12-01 18:32:45 -0500

    finish 2.1

--- src/signing_the_unsigned.cpp
+++ src/signing_the_unsigned.cpp
@@ -1,6 +1,12 @@
 #include "../include/signing_the_unsigned.h"
+#include "igl/knn.h"
+#include "igl/octree.h"
 
 
+#include <Eigen/Core>
+
+using namespace Eigen;
+using namespace std;
 
 void signing_the_unsigned(
     const Eigen::MatrixXd & P,
@@ -8,4 +14,27 @@ void signing_the_unsigned(
     Eigen::MatrixXi & F)
 {
 	// code here
+
+	// compute K-nn
+	// K ranges from 12 - 30
+	int K = 19;
+	vector<vector<int> > O_PI;
+	MatrixXi O_CH;
+	MatrixXd O_CN;
+	VectorXd O_W;
+	MatrixXi KNN;
+	igl::octree(P, O_PI, O_CH, O_CN, O_W);
+	igl::knn(P, K, O_PI, O_CH, O_CN, O_W, I);
+
+	// compute unsigned distance
+	VectorXd dU;
+	dU.resize(P.rows());
+	for(int i = 0; i < P.rows(); i++){
+		double u = 0;
+		for(int j = 0; j < KNN.row(i); j++){
+			u += P.row(i) - P.row(KNN.row(i, j));
+		}
+		dU(i) = 1.0 / k * sqrt(u);
+		
+	}
 }
