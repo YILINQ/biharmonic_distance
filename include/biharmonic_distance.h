a3301f5cd6d7a8d47b7351d00eb947473a12b280
Lily Zhang <junya.zhang@mail.utoronto.ca>
2020-12-08 15:56:47 -0800

    code cleanup

--- include/biharmonic_distance.h
+++ include/biharmonic_distance.h
@@ -1,5 +1,5 @@
 // Given a surface mesh (V,F) compute the biharmonic distance between all
-// pairs of vertices
+// pairs of vertices.
 //
 // Inputs:
 //   V  #V by 3 list of vertex positions
@@ -7,7 +7,6 @@
 // Outputs:
 //   D  symmetric #V by #V matrix of distances where an entry (i,j) 
 //      is the distance between vertex i and j
-// 
 
 #include <Eigen/Core>
 
@@ -15,4 +14,3 @@ void biharmonic_distance(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd &D);
-
--- src/biharmonic_distance.cpp
+++ src/biharmonic_distance.cpp
@@ -1,64 +1,51 @@
 #include "../include/biharmonic_distance.h"
-#include "igl/massmatrix.h"
 #include "igl/cotmatrix.h"
 #include "igl/invert_diag.h"
-#include "igl/pinv.h"
-#include "igl/eigs.h"
-
+#include "igl/massmatrix.h"
 #include <Eigen/Eigenvalues>
 
 using namespace Eigen;
 using namespace std;
-using namespace igl;
-
-
 
 void biharmonic_distance(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd &D)
 {
-	int N = V.rows();
+  int N = V.rows();
 
-	D.resize(N, N);
-	SparseMatrix<double> A, A_inv, Lc;
-	igl::massmatrix(V, F, MASSMATRIX_TYPE_BARYCENTRIC, A);
-	igl::invert_diag(A, A_inv);
-	igl::cotmatrix(V, F, Lc);
+  SparseMatrix<double> A, A_inv, Lc;
+  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_BARYCENTRIC, A);
+  igl::invert_diag(A, A_inv);
+  igl::cotmatrix(V, F, Lc);
 
+  SparseMatrix<double> Ld;
+  Ld = A_inv * Lc;
 
-	SparseMatrix<double> Ld;
-	Ld = A_inv * (Lc);
+  // compute Lc * A.inverse * Lc
+  MatrixXd LcA_Lc = Lc * Ld;
+  LcA_Lc.row(0).setZero();
+  LcA_Lc.col(0).setZero();
+  LcA_Lc(0, 0) = 1;
 
-	// compute Lc * A.inverse * Lc.inverse
-	MatrixXd LcA_Lc_ = (Lc) * Ld;
-	
-	LcA_Lc_.row(0).setZero();
-	LcA_Lc_.col(0).setZero();
+  // construct J matrix (from paper)
+  MatrixXd J;
+  J.resize(N, N);
+  Eigen::VectorXd Ones = Eigen::VectorXd::Ones(N);
+  J = Eigen::MatrixXd::Identity(N, N) - (1.0 / N) * (Ones * Ones.transpose());
+  J.row(0).setZero();
 
-	LcA_Lc_(0, 0) = 1;
+  // now LcA_Lc is invertible
+  MatrixXd Gd;
+  Gd = LcA_Lc.llt().solve(J);
 
+  VectorXd off = (1.0 / N) * Gd.colwise().sum();
+  MatrixXd offset = (off * Ones.transpose()).transpose();
+  Gd -= offset;
 
-	// make J matrix
-	MatrixXd J;
-	J.resize(N, N);
-	Eigen::VectorXd Ones = Eigen::VectorXd::Ones(N);
-	J = Eigen::MatrixXd::Identity(N, N) - (1.0 / N) * (Ones * Ones.transpose());
-	J.row(0).setZero();
-
-
-	// now LcA_Lc_ is invertible
-	MatrixXd Gd;
-	Gd = LcA_Lc_.llt().solve(J);
-
-	VectorXd off = (1.0 / N) * Gd.colwise().sum();
-	MatrixXd offset = (off * Ones.transpose()).transpose();
-	Gd -= offset;
-
-	// dist D(i, j)^2 = Gd(i, i) + Gd(j, j) - 2*Gd(i, j)
-	// set D;
-	VectorXd diag = Gd.diagonal();
-	MatrixXd dd = diag * Ones.transpose();
-	D = sqrt((dd + dd.transpose() - 2*Gd).array());
-
-}	
+  // dist D(i, j)^2 = Gd(i, i) + Gd(j, j) - 2 * Gd(i, j)
+  VectorXd diag = Gd.diagonal();
+  MatrixXd dd = diag * Ones.transpose();
+  D.resize(N, N);
+  D = sqrt((dd + dd.transpose() - 2 * Gd).array());
+}
